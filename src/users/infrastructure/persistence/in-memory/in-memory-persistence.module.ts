632dee2952a68a2d2c9109feeec3a1ae3d0ca5a4
Yazan420 <yazanmahfooz8@gmail.com>
2026-05-09 13:48:48 +0300

    ..

--- src/users/infrastructure/persistence/in-memory/in-memory-persistence.module.ts
+++ src/users/infrastructure/persistence/in-memory/in-memory-persistence.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserRepository } from 'src/users/application/ports/user.repository';
-import { UserFactory } from 'src/users/domain/factories/user.factory';
 import { InMemoryUserRepository } from './repositories/in-memory-user.repository';
 import { InMemoryUserMapper } from './mappers/in-memory-user.mapper';
 
 @Module({
   providers: [
     InMemoryUserMapper,
-    UserFactory,
     {
       provide: UserRepository,
       useClass: InMemoryUserRepository,
