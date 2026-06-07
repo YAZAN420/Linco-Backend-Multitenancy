5b7a42d217a64b1c58fd1068f9dee1a46b2adc99
Yazan420 <yazanmahfooz8@gmail.com>
2026-06-07 16:55:35 +0300

    fix

--- src/demos/application/demo-members-query.service.ts
+++ src/demos/application/demo-members-query.service.ts
@@ -3,24 +3,34 @@ import { DemoMemberQueryRepository } from './ports/demo-member-query.repository'
 import { CursorPageDto } from 'src/common/dtos/pagination/cursor/cursor-page.dto';
 import { DemoMember as PrismaDemoMember } from 'src/generated/prisma/client';
 import { FindDemoMembersCursorQuery } from './interfaces/find-demos.query';
+import { DemoQueryRepository } from './ports/demo-query.repository';
 
 @Injectable()
 export class DemoMembersQueryService {
   constructor(
     private readonly demoMemberQueryRepository: DemoMemberQueryRepository,
+    private readonly demoQueryRepository: DemoQueryRepository,
   ) {}
 
   async findAllByDemo(
     demoId: string,
     options: FindDemoMembersCursorQuery,
   ): Promise<CursorPageDto<PrismaDemoMember>> {
-    return this.demoMemberQueryRepository.findAllByDemo(demoId, options);
+    const demo = await this.demoQueryRepository.findById(demoId);
+    if (!demo) {
+      throw new NotFoundException('Demo not found');
+    }
+    return await this.demoMemberQueryRepository.findAllByDemo(demoId, options);
   }
 
   async findByDemoAndUser(
     demoId: string,
     userId: string,
   ): Promise<PrismaDemoMember> {
+    const demo = await this.demoQueryRepository.findById(demoId);
+    if (!demo) {
+      throw new NotFoundException('Demo not found');
+    }
     const member = await this.demoMemberQueryRepository.findByDemoAndUser(
       demoId,
       userId,
