70241ef3e8d0349d00d54b1d84df4d0bbb3c1cff
Yazan420 <yazanmahfooz8@gmail.com>
2026-06-16 23:55:58 +0300

    ..

--- src/assets/presentation/http/dto/create-asset.dto.ts
+++ src/assets/presentation/http/dto/create-asset.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsNotEmpty, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 import { CreateAssetInput } from 'src/assets/application/interfaces/create-asset-input.interface';
 import { AccessMethod } from 'src/assets/domain/enums/access-method.enum';
 
@@ -7,7 +7,5 @@ export class CreateAssetDto implements CreateAssetInput {
   @IsNotEmpty()
   courseId!: string;
 
-  @IsNotEmpty()
-  @IsEnum(AccessMethod)
-  accessMethod!: AccessMethod;
+  accessMethod: AccessMethod = AccessMethod.PURCHASED;
 }
