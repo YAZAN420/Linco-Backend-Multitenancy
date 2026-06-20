3bd15c6139da9a989e364f8de6e1a2ed36d147e0
MohammadHaboosh <mohammadhaboosh25@gmail.com>
2026-06-20 11:33:47 +0300

    ..

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
       setupSwagger(app);
     }
 
-    await app.listen(port, '209.38.37.231');
+    await app.listen(port, '0.0.0.0');
 
     logger.log(`🚀 Application is running on: http://localhost:${port}`);
     if (swaggerEnabled) {
