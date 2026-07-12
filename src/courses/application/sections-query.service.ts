19556fa9b7912b95fcfa14608140567eb5397dbf
Yazan420 <yazanmahfooz8@gmail.com>
2026-07-13 00:12:16 +0300

    ..

--- src/courses/application/sections-query.service.ts
+++ src/courses/application/sections-query.service.ts
@@ -4,23 +4,27 @@ import { CursorPageDto } from 'src/common/dtos/pagination/cursor/cursor-page.dto
 import { FindSectionsCursorQuery } from './interfaces/find-sections.query';
 import { Section } from 'src/generated/prisma/client';
 import { CourseQueryRepository } from './ports/course-query.repository';
+import { CourseCommandRepository } from './ports/course-command.repository';
 
 @Injectable()
 export class SectionsQueryService {
-  constructor(private readonly courseQueryRepository: CourseQueryRepository) {}
+  constructor(
+    private readonly courseQueryRepository: CourseQueryRepository,
+    private readonly courseCommandRepository: CourseCommandRepository,
+  ) {}
 
   async findAllCursor(
     courseId: string,
     options: FindSectionsCursorQuery,
   ): Promise<CursorPageDto<Section>> {
-    const course = await this.courseQueryRepository.findById(courseId);
+    const course = await this.courseCommandRepository.findById(courseId);
     if (!course) throw new NotFoundException('Course not found');
 
     return this.courseQueryRepository.findSectionsCursor(courseId, options);
   }
 
   async findById(courseId: string, sectionId: string): Promise<Section> {
-    const course = await this.courseQueryRepository.findById(courseId);
+    const course = await this.courseCommandRepository.findById(courseId);
     if (!course) throw new NotFoundException('Course not found');
 
     const section = await this.courseQueryRepository.findSectionById(sectionId);
